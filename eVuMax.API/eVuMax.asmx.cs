0481787ecd9cdc40837bfd99d393b41682748796
etechprathmesh <78482971+etechprathmesh@users.noreply.github.com>
2021-10-28 11:07:06 +0530

    Nitin Changes

--- eVuMax.API/eVuMax.asmx.cs
+++ eVuMax.API/eVuMax.asmx.cs
@@ -111,11 +111,11 @@ namespace eVuMax.API
                
 
 
-                if (!objDataService.OpenConnection(__username, __password, __servername))
+                if (!objDataService.OpenConnectionSqlPassword(__username, __password, __servername))
                 {
                     //Error opening database connection
                     BrokerResponse objBadResponse = objRequest.createResponseObject();
-                    objBadResponse.RequestSuccessfull = true;
+                    objBadResponse.RequestSuccessfull = false;
                     objBadResponse.Errors = "Error opening database connection. \n " + objDataService.LastError;
                     HttpContext.Current.Response.Write(JsonConvert.SerializeObject(objBadResponse));
                 }
@@ -153,7 +153,7 @@ namespace eVuMax.API
                 {
 
                     BrokerResponse objResponse = new BrokerResponse();
-                    objResponse.RequestSuccessfull = true;
+                    objResponse.RequestSuccessfull = false;
                     objResponse.Errors = "Error parsing request. " + ex.Message + ex.StackTrace;
                     //  return JsonConvert.SerializeObject(objResponse);
                     HttpContext.Current.Response.Write(JsonConvert.SerializeObject(objResponse));
