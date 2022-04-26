2e2ade9c4e0f7d808e8717d39783dba8d942cc70
Nishant Vyas <nishantvyas@etechinter.com>
2022-04-26 12:35:13 +0530

    DataBroker: New DataService Module

---
+++ eVuMax.DataBroker/DataServiceManager/DataServiceBroker.cs
@@ -0,0 +1,77 @@
+﻿using eVuMax.DataBroker.Broker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eVuMax.DataBroker.DataServiceManager
+{
+    public class DataServiceBroker : IBroker
+    {
+        BrokerResponse IBroker.getData(BrokerRequest paramRequest)
+        {
+            try
+            {
+
+                if (paramRequest.Broker == Global.Brk_DataServie)
+                {
+
+
+                    DataServiceMgr objMgr = new DataServiceMgr();
+                    return objMgr.getData(paramRequest);
+
+                   
+                }
+
+
+                Broker.BrokerResponse objResponse = paramRequest.createResponseObject();
+                objResponse.RequestSuccessfull = false;
+                objResponse.Errors = "Invalid request Broker header. Please use proper header in the Broker request";
+                return objResponse;
+
+            }
+            catch (Exception ex)
+            {
+
+                Broker.BrokerResponse objResponse = paramRequest.createResponseObject();
+                objResponse.Response = ex.Message + ex.StackTrace;
+                return objResponse;
+
+            }
+
+        }
+
+        BrokerResponse IBroker.performTask(BrokerRequest paramRequest)
+        {
+            try
+            {
+
+                if (paramRequest.Broker == Global.Brk_DataServie)
+                {
+
+
+                    DataServiceMgr objMgr = new DataServiceMgr();
+                    return objMgr.performTask(paramRequest);
+
+
+                }
+
+
+                Broker.BrokerResponse objResponse = paramRequest.createResponseObject();
+                objResponse.RequestSuccessfull = false;
+                objResponse.Errors = "Invalid request Broker header. Please use proper header in the Broker request";
+                return objResponse;
+
+            }
+            catch (Exception ex)
+            {
+
+                Broker.BrokerResponse objResponse = paramRequest.createResponseObject();
+                objResponse.Response = ex.Message + ex.StackTrace;
+                return objResponse;
+
+            }
+        }
+    }
+}
---
+++ eVuMax.DataBroker/DataServiceManager/DataServiceMgr.cs
@@ -0,0 +1,106 @@
+﻿using eVuMax.DataBroker.Broker;
+
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VuMaxDR.Common;
+
+namespace eVuMax.DataBroker.DataServiceManager
+{
+    public class DataServiceMgr:IBroker
+    {
+        const string loadSystemSettings = "loadSystemSettings";
+        const string SaveSystemSettings = "SaveSystemSettings";
+        
+
+        public eVuMaxLogger.eVuMaxLogger objLogger = new eVuMaxLogger.eVuMaxLogger();
+
+        public BrokerResponse getData(BrokerRequest paramRequest)
+        {
+            try
+
+
+            {
+               BrokerResponse objResponse = paramRequest.createResponseObject();
+
+                if (paramRequest.Function == loadSystemSettings)
+                {
+                    VuMaxDR.Common.SystemSettings objSystemSettings = new VuMaxDR.Common.SystemSettings();
+                    objSystemSettings =  SystemSettings.loadSystemSettings(ref paramRequest.objDataService);
+                    objResponse.RequestSuccessfull = true;
+                    objResponse.Response = JsonConvert.SerializeObject(objSystemSettings);
+                    return objResponse;
+
+                }
+
+                return objResponse;
+
+            }
+            catch (Exception ex)
+            {
+                BrokerResponse objResponse = paramRequest.createResponseObject();
+                objResponse.RequestSuccessfull = false;
+                objResponse.Warnings = ex.Message;
+                objResponse.Response = ex.Message + ex.StackTrace;
+                return objResponse;
+                
+            }
+        }
+
+        public BrokerResponse performTask(BrokerRequest paramRequest)
+        {
+            try
+            {
+                BrokerResponse objResponse = paramRequest.createResponseObject();
+
+                if (paramRequest.Function == SaveSystemSettings)
+                {
+
+
+                    string userID = "";
+                    string strSetings = "";
+                    Dictionary<string, SettingValue> objSettings = new Dictionary<string, SettingValue>();
+
+
+
+                    userID = paramRequest.Parameters.Where(x => x.ParamName.Contains("UserID")).FirstOrDefault().ParamValue.ToString();
+                    strSetings = paramRequest.Parameters.Where(x => x.ParamName.Contains("Settings")).FirstOrDefault().ParamValue.ToString();
+                    
+                    if(strSetings != "")
+                    {
+                        try
+                        {
+                            //objSettings = JsonConvert.DeserializeObject<SettingValue>(strSetings);
+                            objSettings = (Dictionary<string, SettingValue>)JsonConvert.DeserializeObject(strSetings);
+                        }
+                        catch (Exception ex)
+                        {
+                            objSettings = new Dictionary<string, SettingValue>();
+
+                        }
+                    }
+                                     
+                    SystemSettings.saveSystemSettings(ref paramRequest.objDataService, objSettings);
+                    objResponse.RequestSuccessfull = true;
+                    objResponse.Response = "";
+                    return objResponse;
+
+                }
+
+                return objResponse;
+
+            }
+            catch (Exception ex)
+            {
+                BrokerResponse objResponse = paramRequest.createResponseObject();
+                objResponse.RequestSuccessfull = false;
+                objResponse.Warnings = ex.Message;
+                objResponse.Response = ex.Message + ex.StackTrace;
+                return objResponse;
+            }
+        }
+    }
+}
