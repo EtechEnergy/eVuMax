dcced6961eb54ede4926a52067d28f1e91f5af35
etechtrainee <vimalkotadiya9307@gmail.com>
2022-05-02 15:52:36 +0530

    changes

--- eVuMax.UI/evumax-web-app/src/components/DataService/Setup/Units/UnitDictionary.tsx
+++ eVuMax.UI/evumax-web-app/src/components/DataService/Setup/Units/UnitDictionary.tsx
@@ -1,23 +1,32 @@
+import axios from "axios";
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Button } from '@progress/kendo-react-all';
 import { Grid, GridToolbar, GridColumn } from '@progress/kendo-react-grid';
 import React, { Component } from 'react'
 import { confirmAlert } from 'react-confirm-alert';
+import BrokerParameter from '../../../../broker/BrokerParameter';
+import BrokerRequest from '../../../../broker/BrokerRequest';
+import GlobalMod from '../../../../objects/global';
+
+let _gMod = new GlobalMod();
+let objBrokerRequest = new BrokerRequest();
+let objParameter = new BrokerParameter("odata", "odata");
 
 export default class UnitDictionary extends Component {
-    state={
+    state = {
         grdUnit: [] as any,
+        UnitID: "",
     }
     ID = "";
     cmdEditUnit = async (event: any, rowData: any) => {
         try {
             debugger;
-            
+
             this.ID = rowData.ID;
             await this.setState({
-                
-                
+
+
 
 
                 //showChartDialog: false,
@@ -31,7 +40,69 @@ export default class UnitDictionary extends Component {
         }
 
     }
+    componentDidMount() {
+        this.LoadUnitList();
 
+
+    }
+    LoadUnitList = () => {
+        //LoadUnitList
+        try {
+            debugger
+            let objBrokerRequest = new BrokerRequest();
+            objBrokerRequest.Module = "DataService";
+            objBrokerRequest.Broker = "UnitSetup";
+            objBrokerRequest.Function = "LoadUnitList";
+
+            //  let objParameter: BrokerParameter = new BrokerParameter("", _gMod._userId);
+            // objBrokerRequest.Parameters.push(objParameter);
+
+
+            // objParameter = new BrokerParameter("UNIT_ID", this.state.UnitID);
+            // objBrokerRequest.Parameters.push(objParameter);
+
+            axios
+                .get(_gMod._getData, {
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json;charset=UTF-8",
+                    },
+                    params: { paramRequest: JSON.stringify(objBrokerRequest) },
+                })
+                .then((res) => {
+                    debugger;
+                    const objData = JSON.parse(res.data.Response);
+                    debugger;
+                    let UnitList = Object.values(objData.units);
+                    console.log(objData);
+                    //UnitCategory: ""
+                    //UnitID: "0.01 deg/ft"
+                    //UnitName: "0.01 deg/ft"
+                    this.setState({ grdUnit: UnitList });
+
+
+                })
+                .catch((error) => {
+                    if (error.response) {
+                        // return <CustomeNotifications Key="success" Icon={false}  />
+                        // this.errors(error.response.message);
+                    } else if (error.request) {
+                        // return <CustomeNotifications Key="success" Icon={false}  />
+                        console.log("error.request");
+                    } else {
+                        // return <CustomeNotifications Key="success" Icon={false}  />
+                        console.log("Error", error);
+                    }
+                    // return <CustomeNotifications Key="success" Icon={false}  />
+                    console.log("rejected");
+                    this.setState({ isProcess: false });
+                });
+
+
+        } catch (error) {
+
+        }
+    }
     cmdRemoveUnit = (event: any, rowData: any) => {
 
         confirmAlert({
@@ -63,94 +134,96 @@ export default class UnitDictionary extends Component {
     }
 
 
-  render() {
-    return (
-      <>
-      <div className="col-6">
-      <Grid
-      // data={[{ SetupId: 'Rig2' }, { RigName: 'Shelf Drilling 0' }, { RigName: 'Rig2' }]}
-      data={this.state.grdUnit}
-  >
-      <GridToolbar>
-          <span>
-              <Button onClick={() => {
-                  {
-                      let Edited = this.state.grdUnit;
+    render() {
+        return (
+            <>
+                <div className="col-6">
+                    <Grid
+                        // data={[{ SetupId: 'Rig2' }, { RigName: 'Shelf Drilling 0' }, { RigName: 'Rig2' }]}
+                        data={this.state.grdUnit}
+                    >
+                        <GridToolbar>
+                            <span>
+                                <Button onClick={() => {
+                                    {
+                                        let Edited = this.state.grdUnit;
 
-                      // let newRow = {
-                      //     SetupId: 0,
-                      //     RigName: 0,
-                      //     SrNo: 0,
-                      // };
+                                        // let newRow = {
+                                        //     SetupId: 0,
+                                        //     RigName: 0,
+                                        //     SrNo: 0,
+                                        // };
+                                        // UnitCategory: ""
+                                        // UnitID: "0.01 deg/ft"
+                                        // UnitName: "0.01 deg/ft"
 
 
+                                        // if (this.state.grdRigNameList.length > 0) {
+                                        //     // newRow.StartMD = editData[this.state.GeoDrlgWindowData.length-1].StartMD+1;
+                                        //     newRow.SrNo = this.state.grdRigNameList.length + 1;
+                                        // } else {
+                                        //     newRow.SrNo = 1;
 
-                      // if (this.state.grdRigNameList.length > 0) {
-                      //     // newRow.StartMD = editData[this.state.GeoDrlgWindowData.length-1].StartMD+1;
-                      //     newRow.SrNo = this.state.grdRigNameList.length + 1;
-                      // } else {
-                      //     newRow.SrNo = 1;
+                                        // }
+                                        // Edited.push(newRow);
+                                        // this.setState({
+                                        //     grdRigNameList: Edited
+                                        // })
 
-                      // }
-                      // Edited.push(newRow);
-                      // this.setState({
-                      //     grdRigNameList: Edited
-                      // })
+                                    }
+                                }}  >Add</Button>
+                            </span>
+                        </GridToolbar>
+                        <GridColumn
+                            field="UnitID"
+                            title="Unit ID"
+                        />
+                        <GridColumn
+                            field="UnitName"
+                            title="Unit Name"
+                        />
+                        <GridColumn
+                            field="UnitCategory"
+                            title="Category"
+                        />
 
-                  }
-              }}  >Add</Button>
-          </span>
-      </GridToolbar>
-      <GridColumn
-          field="UnitID"
-          title="Unit ID"
-      />
-      <GridColumn
-          field="UNIT_NAME"
-          title="Unit Name"
-      />
-      <GridColumn
-          field="CATEGORY"
-          title="Category"
-      />
+                        <GridColumn width="70px" headerClassName="text-center" resizable={false} locked={true}
+                            field="EditUnit"
+                            title="*"
+                            cell={props => (
+                                <td
+                                    style={props.style}
+                                    className={"text-center k-command-cell " + props.className}
+                                >
+                                    {/* <td className={this.props.className + " k-command-cell"} style={this.props.style}> */}
+                                    <span onClick={e => this.cmdEditUnit(e, props.dataItem)}>
+                                        <FontAwesomeIcon icon={faPen} />
+                                    </span>
 
-      <GridColumn width="70px" headerClassName="text-center" resizable={false} locked={true}
-          field="EditUnit"
-          title="*"
-          cell={props => (
-              <td
-                  style={props.style}
-                  className={"text-center k-command-cell " + props.className}
-              >
-                  {/* <td className={this.props.className + " k-command-cell"} style={this.props.style}> */}
-                  <span onClick={e => this.cmdEditUnit(e, props.dataItem)}>
-                      <FontAwesomeIcon icon={faPen} />
-                  </span>
+                                </td>
+                            )}
+                        />
+                        <GridColumn width="70px" headerClassName="text-center" resizable={false} locked={true}
+                            field="removeUnit"
+                            title="*"
+                            cell={props => (
+                                <td
+                                    style={props.style}
+                                    className={"text-center k-command-cell " + props.className}
+                                >
+                                    {/* <td className={this.props.className + " k-command-cell"} style={this.props.style}> */}
+                                    <span onClick={e => this.cmdRemoveUnit(e, props.dataItem)}>
+                                        <FontAwesomeIcon icon={faTrash} />
+                                    </span>
 
-              </td>
-          )}
-      />
-      <GridColumn width="70px" headerClassName="text-center" resizable={false} locked={true}
-          field="removeUnit"
-          title="*"
-          cell={props => (
-              <td
-                  style={props.style}
-                  className={"text-center k-command-cell " + props.className}
-              >
-                  {/* <td className={this.props.className + " k-command-cell"} style={this.props.style}> */}
-                  <span onClick={e => this.cmdRemoveUnit(e, props.dataItem)}>
-                      <FontAwesomeIcon icon={faTrash} />
-                  </span>
+                                </td>
+                            )}
+                        />
 
-              </td>
-          )}
-      />
+                    </Grid>
+                </div>
 
-  </Grid>
-      </div>
-     
-      </>
-    )
-  }
+            </>
+        )
+    }
 }
